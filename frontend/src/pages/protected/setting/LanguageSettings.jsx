eaf70120338b27afaa209b191c2a949ce172ab4d
sanikant kushwaha <kushsani508@gmail.com>
2026-08-15 15:14:08 +0545

    Fix case-sensitive i18n import paths for Linux builds

--- frontend/src/pages/protected/setting/LanguageSettings.jsx
+++ frontend/src/pages/protected/setting/LanguageSettings.jsx
@@ -9,7 +9,7 @@ import {
 } from '@mui/material';
 import { TranslateTwoTone } from '@mui/icons-material';
 import { useTranslation } from 'react-i18next';
-import i18n from '../../../i18n/i18n';
+import i18n from '../../../i18n/I18n';
 import CustomCard from '../../../components/custom/CustomCard';
 
 const LanguageSettings = () => {
